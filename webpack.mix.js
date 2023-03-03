8454ea0f79186a99acac992d947185fd324251d4
mohmohtheinzaw <mohmohtheinzaw>
2023-03-03 14:40:11 +0630

    fix conflict

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,11 +6,21 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
+<<<<<<< HEAD
  | for your Laravel application. By default, we are compiling the Sass
+=======
+ | for your Laravel applications. By default, we are compiling the CSS
+>>>>>>> b1f5e3b670ccc45361d6f991c0b62736fa83437e
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
+<<<<<<< HEAD
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
+=======
+    .postCss('resources/css/app.css', 'public/css', [
+        //
+    ]);
+>>>>>>> b1f5e3b670ccc45361d6f991c0b62736fa83437e
